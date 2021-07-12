274cb6157e7080a0ed7f22171afc8a997ace13ff
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-07-12 17:32:16 -0300

    add process env for database

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://laugusto:9ifCMIZVPOjUD0qN@cluster0.tpisc.mongodb.net/test',
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URL}/${process.env.MONGO_DB}`,
     ),
     UsersModule,
   ],
