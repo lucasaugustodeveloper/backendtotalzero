0d2eaa965c8170caf67b4ee10bc7dbeb9837b704
Lucas Augusto <lucas.augusto5061@gmail.com>
2021-10-04 14:43:27 -0300

    add package dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -6,11 +8,12 @@ import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URL}/${process.env.MONGO_DB}`,
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_URL}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
     ),
     UsersModule,
   ],
   controllers: [],
   providers: [],
 })
+
 export class AppModule { }
